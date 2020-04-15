1694eeba4cb7594994843c91962a29af8c618bdf
lrtte <360589261@qq.com>
2020-04-15 09:31:59 +0800

    提交代码

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 //轮播图
+// }
+// .hualianlogo img{
+//     margin-left: 162px;
+//     width: 1185px;
+//     height: 130px;
+//     width: 950px;
+// }
+// .hualianlogo{
+//     background-color: #fef7fe;
+//     height: 130px;
+// }
+// .all-shangpin{
+//     float: left;
+//     width: 140px;
+//     /* background-color: rgb(253, 253, 253); */
+//     height: 30px;
+//     margin-left: 140px;
+// }
+// .all-shangpin:hover{
+// background-color: #fff;
+// }
+// .all-shangpin a{
+//     color: #333;
