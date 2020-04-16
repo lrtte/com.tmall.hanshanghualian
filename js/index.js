d5ef08099b8fc92b57ab8bcc7b37f6ac4f94454c
lrtte <360589261@qq.com>
2020-04-16 14:24:11 +0800

    提交作业

--- js/index.js
+++ js/index.js
@@ -1,24 +1,36 @@
 //轮播图
-// }
-// .hualianlogo img{
-//     margin-left: 162px;
-//     width: 1185px;
-//     height: 130px;
-//     width: 950px;
-// }
-// .hualianlogo{
-//     background-color: #fef7fe;
-//     height: 130px;
-// }
-// .all-shangpin{
-//     float: left;
-//     width: 140px;
-//     /* background-color: rgb(253, 253, 253); */
-//     height: 30px;
-//     margin-left: 140px;
-// }
-// .all-shangpin:hover{
-// background-color: #fff;
-// }
-// .all-shangpin a{
-//     color: #333;
+
+    $(function(){
+        $('.nav').on('click',function(){
+            console.log('11')
+        })
+        $('.title').hover(function(){
+            $(this).find('span').toggleClass('arrow')
+            $(this).toggleClass('awm')
+        })
+    })
+  
+  // 点击文字和小箭头的空白区域
+        // 1. 给title区域添加点击事件
+        // $('.title').on('click',function(){
+        //     // 1.1下的面板进行折叠和展开的切换
+        //     $(this).next().stop().slideToggle()
+        //     // 1.2箭头的方向的切换
+        //     $(this).find('span').toggleClass('arrow')
+        // })
+        // $('.title').hover(function(){
+        //     $(this).css('backgroundColor','white')
+        //     $(this).find('span').toggleClass('arrow')
+        //     // console.log(11)
+        // })
+
+        // $('#title').on('mouseenter',function(){
+        //     console.log(11)
+        // })
+    //   $('.nav ol>.title').on('mouseenter',function(){
+    //       console.log('11')
+    //   })
+
+    //   $('.title').on('mouseenter',function(){
+    //       console.log('11')
+    //   })
